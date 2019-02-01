3bc4c84ef90b4cd6a734df133a1ef2aaebccde99
USER <USER@10.52.33.237>
2019-02-01 14:42:09 +0300

    LabTwoTasks5-113453

---
+++ src/labtwotasks5/LabTwoTasks5.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labtwotasks5;
+
+/**
+ *
+ * @author USER
+ */
+public class LabTwoTasks5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=25;
+        int b=5;
+        
+        int sum =a+b;
+        System.out.println("Sum of the two integers:"+sum);
+        
+        int difference=a-b;
+        System.out.println("Difference of the two integers:"+difference);
+        
+        int product=a*b;
+        System.out.println("Product of the two integers:"+product);
+        
+        int average=(a+b)/2;
+        System.out.println("Average of the two integers:"+average);
+        
+        int distance=a-b;
+        System.out.println("Distance of the two integers:"+distance);
+        
+        int maxInteger=a;
+        System.out.println("Max Integer:"+maxInteger);
+        
+        int minInteger=b;
+        System.out.println("Min Integer"+minInteger);
+        
+        
+    }
+    
+}
